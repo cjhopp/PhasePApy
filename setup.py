352400e471c984a5401d49108e868df915e2bb5d
Sudipta Basak <basaks@gmail.com>
2017-10-15 05:48:17 +1100

    minor fix and drop py37 support

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        # "Programming Language :: Python :: 3.7",
         # add additional supported python versions
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
