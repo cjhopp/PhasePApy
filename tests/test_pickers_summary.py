10804f67897d6dd0fdc3eb87e0c3830b997849c7
sudhirjain <ubuntu@ip-172-31-14-23.ap-southeast-2.compute.internal>
2017-11-08 02:49:11 +0000

    Accumuating stats for AICD, FB( for each band ), and Kurtosis

--- tests/test_pickers_summary.py
+++ tests/test_pickers_summary.py
@@ -30,17 +30,40 @@ def test_picker_summary(event, picker):
    picker_summary_class = picker_class[:picker_index] + 'Summary'
 
    for s in st[:2]:
+      print ("trace = ")
       print (s)
+      print ("")
+
       picket_summary_str = picker_summary_class + \
                            '(' + 'picker(),' + 's)'
       picker_summary = eval(picket_summary_str)
+
+      res = []; res_deriv = []	
+      if 'AICD' in picker_class: 
+          res, aic_deriv = picker_summary.cf._statistics()
+          print ( "AICD Picker = ")
+          print ( "AIC            = {}".format(res))
+          print ( "AIC_deriv      = {}".format(res_deriv))
+      if 'FB' in picker_class:
+          res = picker_summary.cf._statistics()
+          print ( "FB Picker = ")
+          print ( "Each Band Stats = {}".format(res))
+      if 'KT' in picker_class:
+          res = picker_summary.cf._statistics()
+          print ( "KT Picker = ")
+          print ( "kurtosis stats  = {}".format(res))
+      assert len(res) > 0
+      print ("")
       
       threshold = picker_summary.threshold()
       print ( 'threshold = ',threshold )
+      print ("")
       assert len(threshold) > 0
+      print ("")
 
       uncertanity = picker_summary.uncertainty()
       print ( 'uncertainity = ', uncertanity )
+      print ("")
       assert len(uncertanity) >= 0
 
       scnl, picks, trigger,snr = picker_summary.pick_ident()
@@ -48,9 +71,11 @@ def test_picker_summary(event, picker):
       print ( 'picks   = ',picks)
       print ( 'trigger = ',trigger)
       print ( 'snr     = ',snr)
+      print ("")
       assert len(picks) >= 0
       assert len(trigger) >= 0
       assert len(snr) >=0
   
       polarity = picker_summary.polarity()
       print ( 'Polarity    = ',polarity )
+      print ("")
